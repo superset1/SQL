44c26752047c645ef5d6c19833d8895df5bc636b
superset1 <97288043+superset1@users.noreply.github.com>
2022-02-05 18:05:41 +0200

    Update MySQL_Create_db_hosts_for_iperf3.sql

--- MySQL_Create_db_hosts_for_iperf3.sql
+++ MySQL_Create_db_hosts_for_iperf3.sql
@@ -2,15 +2,24 @@
 
 CREATE DATABASE hosts;
 
--- Create table
+-- Create table for host list
 CREATE TABLE hostspeed (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    host_name VARCHAR(30),
-    speed VARCHAR(15),
+    host_name VARCHAR (30) UNIQUE,
+    description VARCHAR (100),
+    created TIMESTAMP DEFAULT,
+    ); 
+
+-- Create table for iperf3
+CREATE TABLE hostspeed (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    host_id VARCHAR (30),
+    speed VARCHAR (15),
     date_check TIMESTAMP,
-    status VARCHAR(15),
-    date_system INT
-    );
+    status VARCHAR (15),
+    date_system INT, 
+    FOREIGN KEY (host_id) REFERENCES hosts (host_name) ON UPDATE CASCADE
+    ); 
 
 -- Insert hosts
 INSERT INTO hostspeed (host_name) VALUES
